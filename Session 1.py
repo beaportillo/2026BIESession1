850f65e7923a33a24c699e43ca8f287c913fc3fc
bea <bea.p.timothy@gmail.com>
2026-01-20 16:59:49 +0100

    My first commit ever

---
+++ Session 1.py
@@ -0,0 +1,23 @@
+print("Hello World")
+print('Hello World')
+print("7+6")
+print(7+6)
+print('And John asked:"How are you?"')
+print("And John asked:'How are you?'")
+print("hello""world")
+# print('"'"'")
+
+print("3 + 4 =", 3 + 4, "and 5 x 6 =", 5*6)
+print("I have $", 100+200, ".")
+print("I have $", 100+200, ".", sep="")
+
+# end is used to tell print what to do after it prints
+print("1", end="")
+print("2", end="!!")
+print("3", end="")
+print("4", end="\n\n") #\n\n\n just does 3 spaces
+print("5", end="")
+print("6", end="")
+
+print(4/2)
+print(2//4)
